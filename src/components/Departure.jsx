f46b81c8c7906eae50d1bd91e4c06f1e0a8fd3b0
silent <marcin.weglowski1@gmail.com>
2023-10-23 19:21:22 +0200

    timeLeft converting

--- src/components/Departure.jsx
+++ src/components/Departure.jsx
@@ -7,7 +7,7 @@ const Departure = ({ departure }) => {
         <div className='mr-3 text-center w-[2.5em] font-bold rounded-md p-2 m-1 shadow-md bg-red-50 text-red-500'>{departure.routeNumber}</div>
         <div className='flex items-center text-slate-500'>{departure.routeName}</div>
       </div>
-      <div className='font-bold text-lg mr-2 text-slate-500'>{departure.estimatedTime}</div>
+      <div className='font-bold text-lg mr-2 text-slate-500'>{departure.timeLeft}</div>
     </div>
   )
 }
--- src/components/Panels.jsx
+++ src/components/Panels.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback, useRef } from 'react'
 import Panel from './Panel';
 
 const Panels = () => {
@@ -11,12 +11,28 @@ const Panels = () => {
       const response = await fetch('http://ckan2.multimediagdansk.pl/delays?stopId=2001');
       const data = await response.json();
 
+      const currentTime = new Date()
+
       // ITERATE THROUGH DEPARTURES
       for (let dep of data.delay) {
+        const estimatedTimeDate = new Date()
+        const [hours, minutes] = dep.estimatedTime.split(':')
+        estimatedTimeDate.setHours(Number(hours))
+        estimatedTimeDate.setMinutes(Number(minutes))
+
+        let timeDifference = Math.round((estimatedTimeDate - currentTime) / (1000 * 60))
+        if (timeDifference === 0) {
+          timeDifference = ">>>"
+        } else if (timeDifference < 0) {
+          timeDifference = "spóźnia się " + String(timeDifference * -1) + "min"
+        } else {
+          timeDifference += "min"
+        }
+
         const departure = {
           routeNumber: dep.routeId,
           routeName: dep.headsign,
-          estimatedTime: dep.estimatedTime,
+          timeLeft: timeDifference,
         };
         fetchedDepartures.push(departure)
       }
@@ -27,12 +43,24 @@ const Panels = () => {
     }
   }
 
+
   useEffect(() => {
-    fetchDepartures()
+    const fetchData = async () => {
+      console.log('fetching')
+      await fetchDepartures();
+    }
+
+    fetchData()
+
+    const intervalId = setInterval(() => {
+      fetchData()
+    }, 15000)
+
+    return () => clearInterval(intervalId)
   }, [])
 
   return (
-    <div className='flex justify-center pt-10'>
+    <div className='flex justify-center pt-10 flex-wrap'>
       <Panel departures={currentDepartures} />
     </div>
   )
